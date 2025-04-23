1d70929c528736e9740a6dce422b8b6e5c3dd276
tare <siltarre@gmail.com>
2025-04-23 16:46:14 +0900

    🔨 select 필터링 추가

--- app/video/[videoId]/page.tsx
+++ app/video/[videoId]/page.tsx
@@ -13,6 +13,14 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from '@/components/ui/pagination';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import VideoEmbed from '@/components/VideoEmbed';
 import getVisiblePages from '@/lib/getVisiblePages';
 import { getYoutubeComments } from '@/lib/getYoutubeComments';
@@ -25,6 +33,11 @@ export default function VideoAnalysisPage() {
     { text: string; sentiment: '긍정' | '부정' | '중립' }[]
   >([]); // 감정 상태 추가
   const [currentPage, setCurrentPage] = useState(1);
+  const [selectValue, setSelectValue] = useState('all');
+  const [selectFilter, setSelectFilter] = useState<
+    { text: string; sentiment: '긍정' | '부정' | '중립' }[]
+  >([]);
+
   const COMMENTS_PER_PAGE = 5;
   const param = useParams();
   const { videoId } = param; // youtubeId 추출
@@ -45,6 +58,7 @@ export default function VideoAnalysisPage() {
       if (comments.length === 0) return;
       const res = await postSentiment({ comments });
       setSentiment(res);
+      setSelectFilter(res);
     };
 
     fetchSentiment();
@@ -54,13 +68,32 @@ export default function VideoAnalysisPage() {
   const 부정수 = sentiment.filter(item => item.sentiment === '부정').length;
   const 중립수 = sentiment.filter(item => item.sentiment === '중립').length;
 
-  const totalPages = Math.ceil(sentiment.length / COMMENTS_PER_PAGE);
-  const paginatedSentiment = sentiment.slice(
+  const totalPages = Math.ceil(selectFilter.length / COMMENTS_PER_PAGE);
+  const paginatedSentiment = selectFilter.slice(
     (currentPage - 1) * COMMENTS_PER_PAGE,
     currentPage * COMMENTS_PER_PAGE
   );
 
   const visiblePages = getVisiblePages(currentPage, totalPages);
+
+  const handleSelectChange = (value: string) => {
+    setSelectValue(value);
+    switch (value) {
+      case 'positive':
+        setSelectFilter(sentiment.filter(item => item.sentiment === '긍정'));
+        break;
+      case 'negative':
+        setSelectFilter(sentiment.filter(item => item.sentiment === '부정'));
+        break;
+      case 'neutral':
+        setSelectFilter(sentiment.filter(item => item.sentiment === '중립'));
+        break;
+      default:
+        setSelectFilter(sentiment);
+    }
+  };
+
+  console.log(selectValue);
 
   return (
     <Container>
@@ -75,7 +108,21 @@ export default function VideoAnalysisPage() {
         <VideoEmbed videoId={String(videoId)} />
         <CommentChart 긍정수={긍정수} 부정수={부정수} 중립수={중립수} />
       </div>
-
+      <div className="flex justify-end mb-4">
+        <Select defaultValue="all" onValueChange={handleSelectChange}>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a fruit" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="all">전체</SelectItem>
+              <SelectItem value="positive">긍정적</SelectItem>
+              <SelectItem value="negative">부정적</SelectItem>
+              <SelectItem value="neutral">중립</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
       <div className="flex flex-col w-full items-center justify-center gap-4">
         {paginatedSentiment.length > 0 && (
           <div className="flex flex-col items-center w-full">
