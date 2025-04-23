248b42e18c0b2941b8c110d82750fdff79294ac0
tare <siltarre@gmail.com>
2025-04-23 11:19:06 +0900

    ✨ 분석페이지 UI

---
+++ app/video/[videoId]/page.tsx
@@ -0,0 +1,8 @@
+export default function VideoAnalysisPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Video Analysis</h1>
+      <p className="text-lg text-gray-500">YouTube 댓글 감정 분석기</p>
+    </div>
+  );
+}
