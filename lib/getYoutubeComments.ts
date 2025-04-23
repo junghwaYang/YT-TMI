5945dde9d9b561fb43dbe9ed667285ddb9b44b17
tare <siltarre@gmail.com>
2025-04-23 10:39:14 +0900

    ✨ 유튜브 코멘트 get api 호출 함수

---
+++ lib/getYoutubeComments.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/commentThreads';
+const YOUTUBE_API_MAX_RESULTS = 100;
+const YOUTUBE_API_PART = 'snippet';
+
+interface YoutubeComment {
+  snippet: {
+    topLevelComment: {
+      id: string;
+      snippet: {
+        textDisplay: string;
+      };
+    };
+  };
+}
+
+export async function getYoutubeComments(
+  videoId: string
+): Promise<YoutubeComment[]> {
+  const { data } = await axios.get(YOUTUBE_API_URL, {
+    params: {
+      part: YOUTUBE_API_PART,
+      videoId,
+      maxResults: YOUTUBE_API_MAX_RESULTS,
+      key: YOUTUBE_API_KEY,
+    },
+  });
+
+  return data.items.map(
+    (item: YoutubeComment) => item.snippet.topLevelComment.snippet.textDisplay
+  );
+}
