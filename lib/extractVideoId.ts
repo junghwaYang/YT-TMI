3a603efb451bf76d22ffa17702336f35d3d01ddb
tare <siltarre@gmail.com>
2025-04-24 10:42:08 +0900

    🔥 안쓰는값 삭제

--- lib/extractVideoId.ts
+++ lib/extractVideoId.ts
@@ -3,7 +3,7 @@ export function extractVideoId(url: string): string | null {
     const parsedUrl = new URL(url);
     const videoId = parsedUrl.searchParams.get('v');
     return videoId;
-  } catch (e) {
+  } catch {
     return null;
   }
 }
